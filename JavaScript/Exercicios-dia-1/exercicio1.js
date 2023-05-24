4e3b55db1b46ad5ff0787dcc5b5c39d72940a7b8
Pedro <pedrohenriquesilva29@outlook.com>
2023-05-23 21:43:38 -0300

    praticando o uso de if/else

---
+++ JavaScript/Exercicios-dia-1/exercicio1.js
@@ -0,0 +1,41 @@
+// Primeiro exercicío
+//Operações Aritiméticas.
+/*
+let = 1 + 1;
+{
+  console.log(let);
+}
+
+let subtracao = 10 - 5;
+{
+  console.log(subtracao);
+}
+
+let multiplicacao = 2 * 5;
+{
+  console.log(multiplicacao);
+}
+
+let Divisao = 14 / 2;
+{
+  console.log(Divisao);
+}
+
+let Módulo = 10 % 100;
+{
+  console.log(Módulo);
+}
+*/
+// Segundo exercicío
+//Utilizar if/else.
+
+let maiorPontuacao = 5.4;
+if (maiorPontuacao >= 9 && maiorPontuacao <= 10) {
+  console.log("Classificado");
+} else if (maiorPontuacao >= 7 && maiorPontuacao <= 8) {
+  console.log("Aguardando segunda chamada");
+} else if (maiorPontuacao >= 0 && maiorPontuacao <= 6) {
+  console.log(
+    "Poxa, infelizmente não foi dessa vez. Mas haverá uma nova oportunidade!"
+  );
+}
