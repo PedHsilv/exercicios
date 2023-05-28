3650819c89f0b523c35baa1034af2792301c93df
Pedro <pedrohenriquesilva29@outlook.com>
2023-05-28 01:37:00 -0300

    função para determinada busca entre os arrays e elements

--- JavaScript/Exercicios-dia-3/praticando.js
+++ JavaScript/Exercicios-dia-3/praticando.js
@@ -15,10 +15,23 @@ function sum(num1, num2) {
 console.log(10 + 35);
 */
 
-//Ou
-
+//Ou, para agregar uma legibilidade ao resultado pode-se atribuir o retorno a uma constante.
+/*
 function sum(num1, num2) {
   return num1 + num2;
 }
 const result = sum(20, 30);
 console.log(result);
+*/
+// Função para retornar um elemento 'true' ou 'false' para uma busca exclusiva no banco de dados.
+//Exemplo:
+/*
+const onliBankCustomers = ["Pedro", "Vitória", "Aninha", "Luciene"];
+const bancoBradescoCustomers = ["Olivia", "Kelly", "Marcos", "Mariane"];
+
+function findElements(array, element) {
+  return array.includes(element);
+}
+console.log(findElements(onliBankCustomers, "Pedro")); //true
+console.log(findElements(bancoBradescoCustomers, "Kelly")); //true
+*/
